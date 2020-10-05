374c6bd1fad18d89a7bd8c4f7781769e89eaca19
Jlius Suweno <jliuss@live.com>
2020-10-05 08:41:17 +0700

    Initial Commit

---
+++ DAY5BIT14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DAY5BIT14
+//
+//  Created by Jlius Suweno on 05/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
